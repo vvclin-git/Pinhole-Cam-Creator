97e4e2b3c4a1226172d56174f701642649dc7613
rd <rd@R1656001489>
2016-04-08 17:30:34 +0800

    加入專案檔案。

---
+++ WpfApplication1/MainWindow.xaml.vb
@@ -0,0 +1,64 @@
+﻿Imports LightTools
+Imports LTLOCATORLib
+
+Class MainWindow
+    Dim ltLoc As New Locator
+    Dim lt As LTAPI
+    Dim js As New LTCOM64.JSNET
+    Dim pID As Integer
+    Dim fov, lookD, arW, arH As Double
+    Dim imgWidth, imgHeight, xNum, yNum, workDist, apSize As Double
+    Sub New()
+        ' 設計工具需要此呼叫。
+        InitializeComponent()
+        pID = 8432
+        lt = ltLoc.GetLTAPI(pID)
+        xNum = 320
+        yNum = 200
+        workDist = 20
+        apSize = 0.01
+        imgHeight = 2 * workDist * Math.Tan((fov * Math.PI / 180) / 2)
+        imgWidth = imgHeight * arW / arH
+        ' 在 InitializeComponent() 呼叫之後加入所有初始設定。
+    End Sub
+    Private Sub ltTest()
+        Dim stat, stat2 As LTReturnCodeEnum
+        stat = lt.Cmd("\V3D")
+        Debug.Print(stat)
+        'lt.Cmd("PlaceCamera " + js.LTCoord3(0, 0, 0) + " " + js.LTCoord3(0, 0, 1))
+        stat2 = lt.Cmd("PlaceCamera " + lt.Coord3(0, 0, 0) + " " + lt.Coord3(0, 5, 0))
+        Debug.Print(stat2)
+    End Sub
+    Private Sub createCamera(fov As Double, lookDist As Double, arW As Double, arH As Double)
+        'Private Sub createCamera()
+        Dim newCamKey As String
+        lt.Cmd("PlaceCamera " + js.LTCoord3(0, 0, 0) + " " + js.LTCoord3(0, 0, 1)) 'create a camera at origin
+        newCamKey = "LENS_MANAGER[1].STUDIO_MANAGER[Studio_Manager].DATABASE[Camera_List].ENTITY[@Last]"
+        lt.DbSet(newCamKey, "Field_Of_View", fov)
+        lt.DbSet(newCamKey, "Look_Distance", lookDist)
+        lt.DbSet(newCamKey, "Aspect_Width", arW)
+        lt.DbSet(newCamKey, "Aspect_Height", arH)
+    End Sub
+    Private Sub createImgPlane(width As Double, height As Double, xNum As Integer, yNum As Integer, workDist As Double, apSize As Double)
+        Dim newDummyPlaneKey As String
+        Dim newReceiverKey As String
+        Dim newLumMeshKey As String
+        js.MakeDummyPlane(lt, 0, 0, -workDist, 0, 0, 1, W:=width, H:=height)
+        newDummyPlaneKey = "LENS_MANAGER[1].COMPONENTS[Components].PLANE_DUMMY_SURFACE[@Last]"
+        js.MakeReceiver(lt, SurfaceName:=newDummyPlaneKey, ReceiverName:="Pinhole Image")
+        newReceiverKey = "LENS_MANAGER[1].ILLUM_MANAGER[Illumination_Manager].RECEIVERS[Receiver_List].SURFACE_RECEIVER[@Last]"
+        lt.DbSet(newReceiverKey + ".SPATIAL_LUM_METER[Spatial_Lum_Meter]", "VirtualAperture", "Yes")
+        lt.DbSet(newReceiverKey + ".SPATIAL_LUM_METER[Spatial_Lum_Meter]", "Distance", workDist)
+        lt.DbSet(newReceiverKey + ".SPATIAL_LUM_METER[Spatial_Lum_Meter]", "Disk_Radius", apSize)
+        lt.DbSet(newReceiverKey + ".BACKWARD[Backward_Simulation]", "Has_Spatial_Luminance", "Yes")
+        newLumMeshKey = newReceiverKey + ".BACKWARD_SPATIAL_LUMINANCE[Spatial_Luminance].SPATIAL_LUMINANCE_MESH[Spatial_Luminance_Mesh]"
+        lt.DbSet(newLumMeshKey, "X_Dimension", xNum)
+        lt.DbSet(newLumMeshKey, "Y_Dimension", yNum)
+    End Sub
+
+    Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
+        Call ltTest()
+        'Call createCamera(50, 10, 4, 3)
+        'Call createImgPlane(imgWidth, imgHeight, xNum, yNum, workDist, apSize)
+    End Sub
+End Class
---
+++ WpfApplication1/My Project/AssemblyInfo.vb
@@ -0,0 +1,59 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Globalization
+Imports System.Resources
+Imports System.Windows
+
+' 組件的一般資訊是由下列的屬性集控制。
+' 變更這些屬性的值即可修改組件的相關
+' 資訊。
+
+' 檢閱組件屬性的值
+
+<Assembly: AssemblyTitle("WpfApplication1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("WpfApplication1")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2016")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: ComVisible(false)>
+
+'為了建置可當地語系化的應用程式，請設定 
+'.vbproj 檔案中 <PropertyGroup> 內的
+'<UICulture>CultureYouAreCodingWith</UICulture>。例如，如果原始程式檔使用美式英文， 
+'請將 <UICulture> 設為 "en-US"。然後取消註解
+'下列 NeutralResourceLanguage 屬性。更新下一行中的 "en-US"，
+'以符合專案檔中的 UICulture 設定。
+
+'<Assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)> 
+
+
+'ThemeInfo 屬性描述任何主題特定和泛型資源字典的位置。
+'第 1 個參數: 主題特定資源字典的位置
+'(用於頁面中找不到資源時， 
+' 或應用程式資源字典中找不到資源時)
+
+'第 2 個參數: 泛型資源字典的位置
+'(用於頁面中找不到資源時， 
+'以及應用程式和任何主題特定資源字典中找不到資源時)
+<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
+
+
+
+'下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
+<Assembly: Guid("122bd23f-1d98-4ddd-802d-f906cc7946fd")>
+
+' 組件的版本資訊由下列四個值所組成: 
+'
+'      主要版本
+'      次要版本
+'      組建編號
+'      修訂編號
+'
+' 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號
+' 指定為預設值: 
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
